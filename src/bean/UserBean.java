766b1587da150a7c3eecafcea15e05542001fad5
Muhammadanasxon Madaminov <muhammadanasxon6694@gmail.com>
2024-01-10 19:30:44 +0500

    UserBean completed

--- src/bean/UserBean.java
+++ src/bean/UserBean.java
@@ -1,5 +1,46 @@
 package bean;
 
-public class UserBean {
-    //TODO
+public class UserBean extends BaseIdBean{
+    private String username;
+    private String password;
+    private Double balance;
+
+
+    public UserBean() {
+    }
+
+    public UserBean(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public UserBean(String username, String password, Double balance) {
+        this.username = username;
+        this.password = password;
+        this.balance = balance;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(Double balance) {
+        this.balance = balance;
+    }
 }
