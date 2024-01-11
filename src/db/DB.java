54e7b0d3d19ce6b8cd74aa65cff9b5d4dd5f5e1f
Muhammadanasxon Madaminov <muhammadanasxon6694@gmail.com>
2024-01-11 12:33:47 +0500

    User method successfully added in DB

--- src/db/DB.java
+++ src/db/DB.java
@@ -11,23 +11,32 @@ public class DB {
     private static final List<UserBean> USER_BEANS =new ArrayList<>();
     private static final List<CarBean> CAR_BEANS =new ArrayList<>();
 
-    public static UserBean addUser(UserBean userBean){
-        //TODO
-
-        return null;
-    }
-
-    public static UserBean getUser(UserBean userBean){
-        //TODO
-
-        return null;
-    }
-
     public static boolean checkUserExistByLogin(String login){
-
+        for (UserBean user : USER_BEANS) {
+            if (user.getUsername().equals(login)) {
+                return true;
+            }
+        }
         return false;
     }
 
+    public static UserBean addUser(UserBean userBean){
+        if(checkUserExistByLogin(userBean.getUsername())) {
+            return null;
+        }
+        userBean.setId(USER_BEANS.size());
+        USER_BEANS.add(userBean);
+        return userBean;
+    }
+
+    public static UserBean getUser(UserBean userBean){
+        for (UserBean user : USER_BEANS) {
+            if (user.getUsername().equals(userBean.getUsername())&&user.getPassword().equals(userBean.getPassword())) {
+                return user;
+            }
+        }
+        return null;
+    }
 
     public static CarBean addCar(CarBean carBean){
         //TODO
