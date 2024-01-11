aff20c90dd7418610e3ce6a84ce0439de1c644c2
AbdullohXayrullayev <abdullohxayrulloyev@gmail.com>
2024-01-11 11:24:40 +0500

    Base ID bean completed

--- src/bean/BaseIdBean.java
+++ src/bean/BaseIdBean.java
@@ -1,5 +1,16 @@
 package bean;
 
 public class BaseIdBean {
-    //TODO:
+    private Integer id;
+
+    public BaseIdBean() {
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
 }
